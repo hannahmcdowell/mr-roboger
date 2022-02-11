deac98d351433f8f9316a2987d9c2580a0b2cf08
Hannah McDowell <hannah.mcdowell1@gmail.com>
2022-02-11 14:48:23 -0800

    Add a header and form input to index.html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,3 @@
-// input: string representing a number
-// output: string with a list of values starting from 0 up to and including the user's input number, with the following substitutions: if the number contains a 3, all digits should be replaced with "Won't you be my neighbor?", if the number contains a 2, all digits should be replaced with "Boop!" and if the number contains a 1, all digits should be replaced with "Beep!". The values in the ouput string should be separated by commas.
-
 function beepBoop(input) {
   input = input.trim();
   regex = new RegExp("\\D");
