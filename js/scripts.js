c312abe93b70a8f6a06dbdc81acab347799217af
Hannah McDowell <hannah.mcdowell1@gmail.com>
2022-02-11 14:17:49 -0800

    Add slight refactoring to beepBoop function for readability.

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,9 @@
 // output: string with a list of values starting from 0 up to and including the user's input number, with the following substitutions: if the number contains a 3, all digits should be replaced with "Won't you be my neighbor?", if the number contains a 2, all digits should be replaced with "Boop!" and if the number contains a 1, all digits should be replaced with "Beep!". The values in the ouput string should be separated by commas.
 
 function beepBoop(input) {
-  regex = new RegExp("\\D");
   input = input.trim();
-  if(input.search(regex) >= 0 || input.length === 0) {
+  regex = new RegExp("\\D");
+  if(input.length === 0 || input.search(regex) >= 0) {
     return "";
   }
   const array = new Array(parseInt(input) + 1);
@@ -22,5 +22,3 @@ function beepBoop(input) {
   }
   return array.join(", ")
 }
-
-console.log(beepBoop("13"));
